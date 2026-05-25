616c1cf9eef5c348ca2a0a1d15e3a91113155e16
80casper08 <pavlo20202020@gmail.com>
2026-05-25 21:30:26 +0300

    Update hard_questions.py

--- hard_questions.py
+++ hard_questions.py
@@ -16,5 +16,219 @@ questions = [
             ("Так", False),
             ("Всеодно", False)
         ]
-    }
+    },
+         {
+        "text": "Вірно проводиться депаналізація плат на фото?",
+        "image": "https://raw.githubusercontent.com/80casper08/suport.bot1/main/images/2.jpg",
+        "options": [
+            ("Так", True),
+            ("Ні", False)
+        ]
+    },
+         {
+        "text": "Продувка плат CAM.008.MBR.001v2 з обох сторін здійснюється перед початком тестування чи після?",
+        "image": "",
+        "options": [
+            ("Перед початком тестування", True),
+            ("Після тестування", False)
+        ]
+    },
+         {
+        "text": "Які плати відвантажуються на стелаж на PKG?",
+        "image": "",
+        "options": [
+            ("Relay, WallSwitch, MultiTransmitter, PSU, MultiTransmitter Fibra", True),
+            ("HUB, WallSwitch, MultiTransmitter", False),
+            ("MotionCam, WallSwitch, MultiTransmitter, MotionProtect", False)
+        ]
+    },
+         {
+        "text": "Перед початком роботи до ТБ Button має бути підключене?",
+        "image": "",
+        "options": [
+            ("Кабель Ethernet", True),
+            ("Кабель живлення 220V", True),
+            ("Type-C USB", False),
+            ("Антена", True)
+        ]
+    },
+    {
+        "text": "Перед початком роботи до ТБ KeyPadPlus має бути підключене",
+        "image": "",
+        "options": [
+            ("Кабель Ethernet", True),
+            ("Кабель живлення 220V", True),
+            ("Type-C USB", True),
+            ("Антена", True)
+        ]
+    },
+         {
+        "text": "Перед початком роботи до ТБ PWB V4 має бути підключене?",
+        "image": "",
+        "options": [
+            ("Кабель Ethernet", True),
+            ("Кабель живлення 220V", True),
+            ("Type-C USB", False),
+            ("Антена", False)
+        ]
+    },
+         {
+        "text": "Перед початком роботи до ТБ MPO MBR має бути підключене?",
+        "image": "",
+        "options": [
+            ("Кабель Ethernet", True),
+            ("Кабель живлення 220V", True),
+            ("Type-C USB", False),
+            ("Антена", True)
+        ]
+    },
+         {
+        "text": "Перед початком роботи до ТБ MPC MWS має бути підключене?",
+        "image": "",
+        "options": [
+            ("Кабель Ethernet", True),
+            ("Кабель живлення 220V", True),
+            ("Type-C USB", False),
+            ("Антена", False)
+        ]
+    },
+         {
+        "text": "На даному фото які Test Point відповідають за перевірку MCU programming на Ajax, DP.011.MBR.001v3_DIP?",
+        "image": "https://raw.githubusercontent.com/80casper08/suport.bot1/main/images/3.jpg",
+        "options": [
+            ("3", True),
+            ("2", False),
+            ("1", False)
+        ]
+    },
+    {
+        "text": "Перед початком тестування дозволено ставити плату на стіл BOT стороною як зазначено на фото??",
+        "image": "https://raw.githubusercontent.com/80casper08/suport.bot1/main/images/1.jpg",
+        "options": [
+            ("Ні", True),
+            ("Так", False),
+            ("Всеодно", False)
+        ]
+    },
+         {
+        "text": "Перед початком зміни, що потрібно зробити?",
+        "image": "",
+        "options": [
+            ("Взяти QR згідно плану", True),
+            ("Під'єднати браслет", True),
+            ("Взяти плати", True),
+            ("Прибрати зайвий інструмент", True),
+            ("Взяти комікс", True)
+        ]
+    },
+         {
+        "text": "На які регіони прошиваються 915 плати?",
+        "image": "",
+        "options": [
+            ("SA", True),
+            ("AU", True),
+            ("IN", False),
+            ("MA", False),
+            ("NA", True)
+        ]
+    },
+         {
+        "text": "Плати Hub BP на Long Test відвантажуються?",
+        "image": "",
+        "options": [
+            ("На палеті та в ящику", True),
+            ("На палеті", False),
+            ("В ящику", False)
+        ]
+    },
+         {
+        "text": "Плати Hub BP на Long Test відвантажуються?",
+        "image": "",
+        "options": [
+            ("На палеті та в ящику", True),
+            ("На палеті", False),
+            ("В ящику", False)
+        ]
+    },
+    {
+        "text": "Перед початком тестування дозволено ставити плату на стіл BOT стороною як зазначено на фото??",
+        "image": "https://raw.githubusercontent.com/80casper08/suport.bot1/main/images/1.jpg",
+        "options": [
+            ("Ні", True),
+            ("Так", False),
+            ("Всеодно", False)
+        ]
+    },
+         {
+        "text": "Плати Hub BP на Long Test відвантажуються?",
+        "image": "",
+        "options": [
+            ("На палеті та в ящику", True),
+            ("На палеті", False),
+            ("В ящику", False)
+        ]
+    },
+         {
+        "text": "Плати Hub BP на Long Test відвантажуються?",
+        "image": "",
+        "options": [
+            ("На палеті та в ящику", True),
+            ("На палеті", False),
+            ("В ящику", False)
+        ]
+    },
+         {
+        "text": "Плати Hub BP на Long Test відвантажуються?",
+        "image": "",
+        "options": [
+            ("На палеті та в ящику", True),
+            ("На палеті", False),
+            ("В ящику", False)
+        ]
+    },
+         {
+        "text": "Плати Hub BP на Long Test відвантажуються?",
+        "image": "",
+        "options": [
+            ("На палеті та в ящику", True),
+            ("На палеті", False),
+            ("В ящику", False)
+        ]
+    },
+    {
+        "text": "Перед початком тестування дозволено ставити плату на стіл BOT стороною як зазначено на фото??",
+        "image": "https://raw.githubusercontent.com/80casper08/suport.bot1/main/images/1.jpg",
+        "options": [
+            ("Ні", True),
+            ("Так", False),
+            ("Всеодно", False)
+        ]
+    },
+         {
+        "text": "Плати Hub BP на Long Test відвантажуються?",
+        "image": "",
+        "options": [
+            ("На палеті та в ящику", True),
+            ("На палеті", False),
+            ("В ящику", False)
+        ]
+    },
+         {
+        "text": "Плати Hub BP на Long Test відвантажуються?",
+        "image": "",
+        "options": [
+            ("На палеті та в ящику", True),
+            ("На палеті", False),
+            ("В ящику", False)
+        ]
+    },
+         {
+        "text": "Плати Hub BP на Long Test відвантажуються?",
+        "image": "",
+        "options": [
+            ("На палеті та в ящику", True),
+            ("На палеті", False),
+            ("В ящику", False)
+        ]
+    },
 ]
